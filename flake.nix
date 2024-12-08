4a0a4f3adc344b9e795f069ef2381fb5ab67e90f
Samuel Carey <sam@scarey.me>
2024-12-07 23:58:42 -0500

    build: add default nix package

--- flake.nix
+++ flake.nix
@@ -3,23 +3,32 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
-  inputs.devenv.url = "github:cachix/devenv";
 
-  outputs = inputs@{ self, devenv, flake-parts, nixpkgs }:
+  outputs = inputs@{ self, flake-parts, nixpkgs }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        devenv.flakeModule
-      ];
-
       systems = [
         "x86_64-linux"
         "aarch64-darwin"
       ];
 
       perSystem = { lib, pkgs, system, ... }: {
-        devenv.shells.default = {
-          languages = {
-            go.enable = true;
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+          ];
+        };
+        packages.default = pkgs.buildGoModule {
+          pname = "amtcli";
+          version = "0.1.0";
+
+          src = ./.;
+
+          vendorHash = "sha256-s32vUZqPayI8CY/SvBzrhj600D/NkElu/d65hlP2+PM=";
+
+          meta = {
+            description = "A command-line Intel AMT tool";
+            homepage = "https://github.com/scareyo/amtcli";
+            license = lib.licenses.mit;
           };
         };
       };
