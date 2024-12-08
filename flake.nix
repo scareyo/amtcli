0bda8f698001f7c3be551ad93ba77a12a33162a9
Samuel Carey <sam@scarey.me>
2024-12-07 23:58:42 -0500

    build: add default nix package

--- flake.nix
+++ flake.nix
@@ -22,6 +22,25 @@
             go.enable = true;
           };
         };
+        packages.default = pkgs.buildGoModule {
+          pname = "amtcli";
+          version = "0.1.0";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "scareyo";
+            repo = "amtcli";
+            rev = "main";
+            sha256 = "01hhy3prm8jbyam1n6qw1nl4ra5lghgafr9cd2wr3568ncdq8369";
+          };
+
+          vendorHash = "sha256-s32vUZqPayI8CY/SvBzrhj600D/NkElu/d65hlP2+PM=";
+
+          meta = {
+            description = "A command-line Intel AMT tool";
+            homepage = "https://github.com/scareyo/amtcli";
+            license = lib.licenses.mit;
+          };
+        };
       };
     };
 }
